ce30b90ab1dbd7ab63ecf8daaa1da48869097a5b
e-s2055 <elizabeth.b.s@protonmail.com>
2025-08-11 08:23:18 -0500

    Added character_count function to stats.py; fetches and converts all characters into lowercase and gets count of characters, symbols, and spaces

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 ### imports function from stats.py 
 from stats import get_num_words
+from stats import character_count
 
 def get_book_text(filepath):
     """
@@ -16,6 +17,8 @@ def main():
     get_book = get_book_text("books/frankenstein.txt")
     num_words = get_num_words(get_book)
     print(f"{num_words} words found in the document")
+    char_count = character_count(get_book)
+    print(f"{char_count}")
 
 ### Executes the program
 main()
--- stats.py
+++ stats.py
@@ -4,3 +4,18 @@ def get_num_words(x):
         """
         words = x.split()
         return len(words)
+
+def character_count(x):
+        """
+        Takes text from book as a string, converts to lowercase, returns number of times characters appear; incl. symbols and spaces
+        """
+        lowercase = ""
+        lowercase = x.lower()
+        character_count = {}
+        for char in lowercase:
+            if char in character_count:
+                   character_count[char] += 1
+            else:
+                   character_count[char] = 1
+        return character_count
+            
