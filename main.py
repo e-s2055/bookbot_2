6ecdd1ed31b6eeaf88ceb82954e258dc62c1a8a8
e-s2055 <elizabeth.b.s@protonmail.com>
2025-08-11 09:06:44 -0500

    Reorganized report structure, sorted from greatest to least per char count

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-### imports function from stats.py 
-from stats import get_num_words
-from stats import character_count
+### imports functions from stats.py 
+from stats import get_num_words, character_count, chars_dict_to_sorted_list
 
 def get_book_text(filepath):
     """
@@ -14,11 +13,24 @@ def main():
     """
     Calls get_book_text() with the relative path "book/frankenstein.txt" and prints the result
     """
-    get_book = get_book_text("books/frankenstein.txt")
+    print("============ BOOKBOT ============")
+    
+    book_path = "books/frankenstein.txt"
+    get_book = get_book_text(book_path)
+    print(f"Analyzing book found at {book_path}...")
+
+    print("----------- Word Count ----------")
     num_words = get_num_words(get_book)
-    print(f"{num_words} words found in the document")
+    print(f"Found {num_words} total words")
+
+    print("--------- Character Count -------")    
     char_count = character_count(get_book)
-    print(f"{char_count}")
+    char_sorted_list = chars_dict_to_sorted_list(char_count)
+    for character in char_sorted_list:
+        if not character["char"].isalpha():
+            continue #Skips non-alpha chars when printing
+        print(f"{character['char']}: {character['num']}")
+    print("============= END ===============")
 
 ### Executes the program
 main()
--- stats.py
+++ stats.py
@@ -18,4 +18,18 @@ def character_count(x):
             else:
                    character_count[char] = 1
         return character_count
-            
+
+def chars_dict_to_sorted_list(x):
+    """
+    Creates 2 dictionaries with key-value for character and character count, sorts from greatest to least count
+    """
+    character_count_sorted = []
+    for char in x:
+          count = x[char] # Get the count that's there already
+          char_dict = {"char": char, "num": count} # Create new formatting
+          character_count_sorted.append(char_dict) # Adds it to the list as a pair
+    character_count_sorted.sort(reverse=True, key=sort_on) # Sorts the list
+    return character_count_sorted
+
+def sort_on(x):
+       return x["num"]
