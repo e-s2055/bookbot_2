eb0c8858b80f30407820663522cf9d190696c26c
e-s2055 <elizabeth.b.s@protonmail.com>
2025-08-11 07:58:52 -0500

    Added word count function, adjusted comment structure to docstrings

--- main.py
+++ main.py
@@ -1,14 +1,26 @@
 
-# get_book_text(filepath): Takes a file path as input, opens the file using a with block, reads all contents into a string, and returns it
 def get_book_text(filepath):
+    """
+    Takes a file path as input, opens the file using a with block, reads all contents into a string, and returns it
+    """
     with open(filepath) as f:
         file_contents = f.read()
         return file_contents
-    
-#main(): Calls get_book_text() with the relative path "book/frankenstein.txt" and prints the result
-def main():
-    get_book = get_book_text("books/frankenstein.txt")
-    print(get_book)
 
-#main(): Executes the program
+def book_word_count(x):
+        """
+        Accepts text from book as string and returns number of words in string
+        """
+        words = x.split()
+        return len(words)
+
+def main():
+    """
+    Calls get_book_text() with the relative path "book/frankenstein.txt" and prints the result
+    """
+    get_book = get_book_text("books/frankenstein.txt")
+    num_words = book_word_count(get_book)
+    print(f"{num_words} words found in the document")
+
+### Executes the program
 main()
